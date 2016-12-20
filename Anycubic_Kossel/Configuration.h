1a7448fc710b0bec26360cb048a6e10f7a94d565
James <jam3sward@users.noreply.github.com>
2016-12-20 23:04:09 +0000

    Raise 15mm before probing first point (otherwise it skims very close to the bed when rapidly traversing to first point)

--- Anycubic_Kossel/Configuration.h
+++ Anycubic_Kossel/Configuration.h
@@ -396,7 +396,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
   #define XY_TRAVEL_SPEED 2000         // X and Y axis travel speed between probes, in mm/min
 
-  #define Z_RAISE_BEFORE_PROBING 10  //How much the extruder will be raised before traveling to the first probing point.
+  #define Z_RAISE_BEFORE_PROBING 15  //How much the extruder will be raised before traveling to the first probing point.
   #define Z_RAISE_BETWEEN_PROBINGS 5  //How much the extruder will be raised when traveling from between next probing points
 
 
